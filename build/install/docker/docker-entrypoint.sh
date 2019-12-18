1d17b76cf9cc82eeb0421b23f701fd922ab81fba
Alisher Ochilov <alisher.ochilov@onlyoffice.com>
2019-12-18 15:15:33 +0000

    add entrypoind

---
+++ build/install/docker/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MYSQLIP=`getent hosts mysql | awk '{ print $1 }'`
+KAFKAIP=`getent hosts kafka | awk '{ print $1 }'`
+
+sed -i "s/localhost/${MYSQLIP}/" /app/onlyoffice/config/appsettings.test.json
+sed -i "s/localhost/${KAFKAIP}/" /app/onlyoffice/config/kafka.test.json
+
+/usr/bin/supervosord --

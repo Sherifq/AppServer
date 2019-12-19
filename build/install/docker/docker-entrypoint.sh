d23bb46b3b911045e6dd2cf8720d173f9694987d
Alisher Ochilov <alisher.ochilov@onlyoffice.com>
2019-12-19 12:46:37 +0000

    fix trouble with init script for resolving hostnames services

--- build/install/docker/docker-entrypoint.sh
+++ build/install/docker/docker-entrypoint.sh
@@ -3,7 +3,11 @@
 MYSQLIP=`getent hosts mysql | awk '{ print $1 }'`
 KAFKAIP=`getent hosts kafka | awk '{ print $1 }'`
 
-sed -i "s/localhost/${MYSQLIP}/" /app/onlyoffice/config/appsettings.test.json
+tmp=$(mktemp)
+
+jq ".ConnectionStrings.default.connectionString=\"Server=${MYSQLIP};Port=3306;Database=${MYSQL_DATABASE};User ID=${MYSQL_USER};Password=${MYSQL_PASSWORD};Pooling=true;Character Set=utf8;AutoEnlist=false;SSL Mode=Required\"" /app/onlyoffice/config/appsettings.test.json > "$tmp" && mv "$tmp" /app/onlyoffice/config/appsettings.test.json
+
+
 sed -i "s/localhost/${KAFKAIP}/" /app/onlyoffice/config/kafka.test.json
 
 /usr/bin/supervisord --

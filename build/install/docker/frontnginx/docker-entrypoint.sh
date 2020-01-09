b54832de1fefccf2192cd4a5c2d78ec9334ba854
Alisher Ochilov <alisher.ochilov@onlyoffice.com>
2020-01-09 11:27:35 +0000

    temporary commit, minor fix

--- build/install/docker/frontnginx/docker-entrypoint.sh
+++ build/install/docker/frontnginx/docker-entrypoint.sh
@@ -10,10 +10,12 @@ PEOPLESERVER_HOSTS_IP=`getent hosts $PEOPLESERVER_HOSTS | awk '{ print $1 }'`
 STUDIO_HOSTS_IP=`getent hosts $STUDIO_HOSTS | awk '{ print $1 }'`
 
 tmp=$(mktemp)
-
 jq ".ConnectionStrings.default.connectionString=\"Server=${MYSQLIP};Port=3306;Database=${MYSQL_DATABASE};User ID=${MYSQL_USER};Password=${MYSQL_PASSWORD};Pooling=true;Character Set=utf8;AutoEnlist=false;SSL Mode=Required\"" /app/onlyoffice/config/appsettings.test.json > "$tmp" && mv "$tmp" /app/onlyoffice/config/appsettings.test.json
+chown 1000:1000 /app/onlyoffice/config/appsettings.test.json
 
-sed -i "s/localhost/${KAFKAIP}/" /app/onlyoffice/config/kafka.test.json
+tmp=$(mktemp)
+jq ".kafka.BootstrapServers=\"${KAFKAIP}\"" /app/onlyoffice/config/kafka.test.json > "$tmp" && mv "$tmp" /app/onlyoffice/config/kafka.test.json
+chown 1000:1000 /app/onlyoffice/config/kafka.test.json
 
 sed -i "s/server\ api/server\ $API_HOSTS_IP/" /etc/nginx/includes/upstream.conf
 sed -i "s/server\ client/server\ $CLIENT_HOSTS_IP/" /etc/nginx/includes/upstream.conf
